e2219f2ccd2d2ddf7fb8af5180941d7287252dfe
mkaraki <mkaraki_apps_publisher@outlook.jp>
2018-04-06 11:44:43 +0900

    fix protect music file

--- code/ui/player/request_music.php
+++ code/ui/player/request_music.php
@@ -5,5 +5,7 @@ $music_path=$sql_host->querySingle("SELECT path FROM song where id = ".$request_
 
 if ($music_path==""){die("e0");}
 if (!file_exists(dirname(__FILE__)."/../../../".$music_path)){die("e1");}
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=".basename("./file/".$_GET['id']));
 readfile(dirname(__FILE__)."/../../../".$music_path);
 ?>
