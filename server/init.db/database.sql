f8730572539d66196bde214db13a44e5d480f05f
mkaraki <masakazu.araki@mkarakiapps.com>
2022-11-10 11:24:16 +0900

    Making new database structure

--- server/init.db/database.sql
+++ server/init.db/database.sql
@@ -11,26 +11,29 @@ CREATE TABLE IF NOT EXISTS sessionToken(
 );
 CREATE TABLE IF NOT EXISTS library(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256),
     basepath TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS accessList(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    userid INT NOT NULL,
+    userid INT,
     libraryId INT NOT NULL,
     permission INT DEFAULT(5),
     FOREIGN KEY(libraryId) REFERENCES library(id),
     FOREIGN KEY (userid) REFERENCES user(id)
 );
 CREATE TABLE IF NOT EXISTS artistMetadata(
-    mbid CHAR(36) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    mbid CHAR(36),
     name TEXT,
     namePhonic TEXT,
     nameAlphabet TEXT,
     imagePath TEXT,
     disambiguation LONGTEXT
 );
-CREATE TABLE IF NOT EXISTS releaseMetadata(
-    mbid CHAR(36) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS release(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    releaseMbid CHAR(36),
     title TEXT,
     titlePhonic TEXT,
     artworkPath TEXT,
@@ -38,33 +41,29 @@ CREATE TABLE IF NOT EXISTS releaseMetadata(
     releaseDate DATE,
     disambiguation LONGTEXT
 );
-CREATE TABLE IF NOT EXISTS trackMetadata(
-    trackMbid CHAR(36) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS track(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    libraryId INT NOT NULL,
     recordingMbid CHAR(36),
-    releaseMbid CHAR(36),
+    releaseId INT,
     title TEXT,
     duration INT,
     diskNo INT NOT NULL DEFAULT(0),
     trackNo INT NOT NULL DEFAULT(0),
-    FOREIGN KEY(releaseMbid) REFERENCES releaseMetadata(mbid)
-);
-CREATE TABLE IF NOT EXISTS track(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    libraryId INT NOT NULL,
-    trackMbid CHAR(36) NOT NULL,
     path TEXT NOT NULL,
     FOREIGN KEY(libraryId) REFERENCES library(id),
-    FOREIGN KEY(trackMbid) REFERENCES trackMetadata(trackMbid)
+    FOREIGN KEY (releaseId) REFERENCES release(id),
 );
 CREATE TABLE IF NOT EXISTS artistMap(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    artistMbid CHAR(36) NOT NULL,
+    type INT NOT NULL DEFAULT(1),
+    artistId INT NOT NULL,
     mapNo INT DEFAULT(0),
     dispName TEXT DEFAULT(NULL),
     joinPhrase TEXT DEFAULT(NULL),
     releaseMbid CHAR(36) DEFAULT(NULL),
     trackMbid CHAR(36) DEFAULT(NULL),
-    FOREIGN KEY(artistMbid) REFERENCES artistMetadata(mbid),
+    FOREIGN KEY(artistId) REFERENCES artistMetadata(id),
     FOREIGN KEY(releaseMbid) REFERENCES releaseMetadata(mbid),
     FOREIGN KEY(trackMbid) REFERENCES trackMetadata(trackMbid)
 );
