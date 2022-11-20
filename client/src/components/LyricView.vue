2d16929ab011a1815cad03095177f6491ff33699
mkaraki <masakazu.araki@mkarakiapps.com>
2022-11-21 01:16:35 +0900

    Add auto scroll to lyric view

--- client/src/components/LyricView.vue
+++ client/src/components/LyricView.vue
@@ -16,6 +16,10 @@ const lyricInfo: Ref<any> = ref([]);
 
 const gotLyric = ref('loading');
 
+const holder: Ref<any> = ref(null);
+
+let currentFocused = 0;
+
 onMounted(() => {
     fetch(baseurl + `/library/${libraryId}/track/` + props.trackId + '/lyric', {
         credentials: 'include'
@@ -24,32 +28,48 @@ onMounted(() => {
         .then(data => {
             lyricInfo.value = data;
             gotLyric.value = 'ok';
+
+            setInterval(function () {
+                const msCurrent = props.currentTime * 1000;
+                const detectedId = lyricInfo.value['lines'].findIndex((v: any) =>
+                    v['time'] <= msCurrent &&
+                    msCurrent < v['endtime']
+                );
+                if (detectedId > 0 && detectedId !== currentFocused) { 
+                    document.getElementById(`lyric-l${(detectedId - 4 < 0) ? (0) : (detectedId - 4)}`)?.scrollIntoView(true);
+                    currentFocused = detectedId;
+                }
+            }, 200);
         })
         .catch(() => {
             gotLyric.value = 'fail';
         });
+
 });
+
 </script>
 
 <template>
-    <div v-if="gotLyric === 'ok'" class="lyricView">
-        <div v-for="(lyricLine, index) in lyricInfo['lines']" :key="index"
-            :class="(lyricLine['time'] < (currentTime * 1000) ? 'playedLyricLine' : 'toPlayLyricLine') + ' ' + (lyricLine['endtime'] < (currentTime * 1000) ? 'endedLyricLine' : '')">
-            <span v-for="(lyricSection, secindex) in lyricLine['sections']" :key="secindex"
-                :class="lyricSection['time'] < (currentTime * 1000) ? 'playedLyric' : 'toPlayLyric'">
-                {{ lyricSection['text'] }}
-            </span>
-        </div>
-    </div>
-    <div v-else-if="gotLyric === 'loading'">
-        <div class="d-flex justify-content-center">
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
+    <div ref="holder">
+        <div v-if="gotLyric === 'ok'" class="lyricView">
+            <div v-for="(lyricLine, index) in lyricInfo['lines']" :key="index" :id="`lyric-l${index}`"
+                :class="(lyricLine['time'] < (currentTime * 1000) ? 'playedLyricLine' : 'toPlayLyricLine') + ' ' + (lyricLine['endtime'] < (currentTime * 1000) ? 'endedLyricLine' : '')">
+                <span v-for="(lyricSection, secindex) in lyricLine['sections']" :key="secindex"
+                    :class="lyricSection['time'] < (currentTime * 1000) ? 'playedLyric' : 'toPlayLyric'">
+                    {{ lyricSection['text'] }}
+                </span>
             </div>
         </div>
-    </div>
-    <div v-else>
-        Failed to fetch lyric.
+        <div v-else-if="gotLyric === 'loading'">
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            Failed to fetch lyric.
+        </div>
     </div>
 </template>
 
